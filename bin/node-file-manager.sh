341b6a85d333994991985bc54abf3395a937cac4
Sylvain Pellier <contact@sponline.fr>
2019-04-10 06:58:29 +0000

    node-file-manager.sh édité en ligne via Bitbucket

--- bin/node-file-manager.sh
+++ bin/node-file-manager.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 SHELL_PATH=`dirname $0`
-cd $SHELL_PATH/../lib/node_modules/node-file-manager/lib
+cd $SHELL_PATH/../lib/node_modules/fork-node-file-manager/lib
 
 node --harmony index.js $*
