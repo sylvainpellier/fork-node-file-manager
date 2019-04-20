d8c2100d1313c40276c3662463036b39db5fd862
Sylvain Pellier <contact@sponline.fr>
2019-04-20 05:25:35 +0000

    fork-node-file-manager.sh édité en ligne via Bitbucket

--- bin/fork-node-file-manager.sh
+++ bin/fork-node-file-manager.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-SHELL_PATH=`dirname $0`
 cd /home/pi/escape_game/node_modules/fork-node-file-manager/lib
 
 node --harmony /home/pi/escape_game/node_modules/fork-node-file-manager/lib/index.js $*
