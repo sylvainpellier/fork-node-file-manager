7aea5b73ecfaf97d8b704d817c162098cfcb7851
Sylvain Pellier <pellier@gmail.com>
2020-02-22 12:56:57 +0100

    Update fork-node-file-manager.sh

--- bin/fork-node-file-manager.sh
+++ bin/fork-node-file-manager.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-cd /home/pi/escape_game/node_modules/fork-node-file-manager/lib
+cd /node_modules/fork-node-file-manager/lib
 
-node --harmony /home/pi/escape_game/node_modules/fork-node-file-manager/lib/index.js $*
+node --harmony /node_modules/fork-node-file-manager/lib/index.js $*
